581591362b295016cdc6e81a1a96f41711236f4d
Alexei1999 <Shulzhickij.AO.17@gmail.com>
2022-05-27 22:58:04 +0500

    fix: minor changes

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -1,7 +1,7 @@
 import { getIsKz } from "@shared/lib/functional-utils";
 import axios from "axios";
 
-export const hostUrl = getIsKz()
+export const hostUrl = !getIsKz()
   ? "https://oceanbasket.ru/oceanBasket/public"
   : "https://oceanbasket.kz/oceanBasket/public";
 
